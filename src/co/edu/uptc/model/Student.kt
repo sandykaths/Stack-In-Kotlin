0907ff72e4808d01220ab87459e831ca6dce1b11
sandykaths <sandykaths073@gmail.com>
2026-03-18 11:52:44 -0500

    Stack Kotlin

--- src/co/edu/uptc/model/Student.kt
+++ src/co/edu/uptc/model/Student.kt
@@ -2,6 +2,6 @@ package co.edu.uptc.model
 
 class Student(val name: String, val code: Int, val semester: Int){
     override fun toString(): String {
-        return "\nnombre del estudiante:($name)\ncódigo:($code)\nsemestre:($semester)"
+        return "\nnombre del estudiante:$name\ncódigo:$code\nsemestre:$semester"
     }
 }
